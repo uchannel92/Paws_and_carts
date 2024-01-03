560f1e3d6f23cbf71b31031c1899b0fa8604c71f
Uchannel92 <68905107+uchannel92@users.noreply.github.com>
2024-01-03 19:41:57 +0000

    Update capstone_project1.py
    Moved the project plan and psuedocode to a txt file and now added the code so far in this .py file

--- capstone_project1.py
+++ capstone_project1.py
@@ -1,6 +1,48 @@
-"""Cats and Paws
+total = 0
 
-Psuedocode
+cart = {
+    'water': 5,
+    'crips': 4,    
+}
+
+user_cart = [{}, {}]
+
+for item, price in cart.items():
+    # item_and_price = f'{item} - {price}'
+    # print(item_and_price)
+
+    total += price
+    price(f"{}")
+
+print(total)
+
+def total_cart(arg1, arg2):
+    pass
 
 
-"""
+def show_cart():
+    pass
+
+def add_to_cart():
+    pass
+
+def remove_from_cart():
+    pass
+
+
+# Serge logic
+shopping_done = False
+while (not shopping_done):
+
+    if 'x' == 'something':
+        shopping_done = True
+        print("Goodbye")
+    pass
+
+# Riaan logic
+user_input = ''
+
+while user_input != 'q':
+
+    if user_input == 'q':
+        break
