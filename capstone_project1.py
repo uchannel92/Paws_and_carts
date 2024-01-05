6e0515a6b67194ebfa7573d4f24183bf714be57f
Uchannel92 <68905107+uchannel92@users.noreply.github.com>
2024-01-05 11:40:42 +0000

    Update capstone_project1.py
    Added:
    list of products and prices
    Created a dictionary of store items
    Added store items to store list
    
    
    Adding functions to display:
    
    Items in store
    User basket

--- capstone_project1.py
+++ capstone_project1.py
@@ -1,27 +1,47 @@
-total = 0
+basket_total_price = 0
+shopping_basket = []
+store_products = []
+animal_products = [
+    "cat food", "dog food", "cat toy",
+    "dog toy", "ball", "collar",
+]
+animal_products_price = [
+    2, 3, 20, 22,
+    7, 6
+]
 
-cart = {
-    'water': 5,
-    'crips': 4,    
-}
+for list_value in range(len(animal_products)):
 
-user_cart = [{}, {}]
+    store_items = {
+        animal_products[list_value]: animal_products_price[list_value],
+    }
+    store_products.append(store_items)   
 
-for item, price in cart.items():
-    # item_and_price = f'{item} - {price}'
-    # print(item_and_price)
+# display the available store items    
+def show_store_items(list):
+    """ display the items in the store and their prices upon request"""
 
-    total += price
-    price(f"{}")
-
-print(total)
+    print('+'*79)
+    print(f"View our {len(list)} products:\n")
+    for product in list: 
+        for prod_info, prod_price in product.items():
+            print(f"Product: {prod_info}\nPrice: £{prod_price}\n")
+    print('+'*79)
+show_store = show_store_items(store_products)
 
 def total_cart(arg1, arg2):
     pass
 
+def display_user_shopping_basket(list):
 
-def show_cart():
-    pass
+    if len(list) < 1:
+        print("You have no items in your cart")
+    else:
+        print("Your shopping cart:\n")
+        for item in list:
+            print(item)
+
+show_basket = display_user_shopping_basket(shopping_basket)
 
 def add_to_cart():
     pass
@@ -30,7 +50,7 @@ def remove_from_cart():
     pass
 
 
-# Serge logic
+"""# Serge logic
 shopping_done = False
 while (not shopping_done):
 
@@ -45,4 +65,4 @@ user_input = ''
 while user_input != 'q':
 
     if user_input == 'q':
-        break
+        break"""
