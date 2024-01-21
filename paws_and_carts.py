950b15e175c2c2d06ccc534ea5bfaa3ad0b0e555
Uchannel92 <68905107+uchannel92@users.noreply.github.com>
2024-01-21 00:04:24 +0000

    Update paws_and_carts.py
    Updated functions, added while loop with options to exit loop.
    
    Need to work on:
    
    Adding user data to a text file
    
    Formatting text file to a dict/list (This decision still needs to be made and currently testing)

--- paws_and_carts.py
+++ paws_and_carts.py
@@ -1,166 +1,98 @@
-def read_basket_file():
-    with open(filename, 'r') as file_obj:
-        file_contents = file_obj.readlines()
-    return file_contents
-
-def format_store_file_contents():
-    for line in file_contents:
-        format_line_content = line.strip().replace(',', ' ').split()
-        product_id = format_line_content[0] # item_n
-        product_name = format_line_content[1].replace('_', ' ') # product name
-        product_price = format_line_content[2] # product price
-        product = [product_id, product_name, product_price]
-        products.append(product) # here i populate the products list.
-
-def populate_store_products_to_dict():
-    for product_index, product in enumerate(products):
-        product_id = products[product_index][0]
-        product_name = products[product_index][1]
-        product_price = products[product_index][2]
-        products_dict[product_id] = [product_name, product_price]
-
-def display_store_products_to_user():
-    print("Our products:")
-    for item, product in products_dict.items():
-        for i in product:
-            catalogue = f"Product Name: {product[0]}\nProduct Price: £{product[1]}"
-            print(catalogue)
-            break
-
-def add_product():
-    user_product_to_add_input = input("Enter what item you want to add: ")
-    return user_product_to_add_input
-
-def validate_user_product(add_item): # this param is the return value from the user add_product func
-    for item in products:
-        store_product = item[1] # we're focused only on product items index.
-        if add_item == store_product:
-            add_item = store_product
-            print(f"Your product:{add_item} == {store_product}")
-            return True
-        
-def obtain_product_and_price(add_item):
-    for item, product in products_dict.items():
-        for i in product: # i is refence to the title of the product.
-            if i == add_item:
-                prod, price = product[0], product[1]
-                return f"{prod}, {price}" # return nested list of product and price.
-
-def write_to_basket_file(add_item):
-    print("Product has been added!")
-    # def add_product() needs to be a value here, the return value from a successful product
-    # basket.append() # the product variable from user
-    # output - user item has been added to basket.
-    filename = "user_basket.txt"
-    with open(filename, 'a') as file_obj:
-        print("Your item has been added to the basket.")
-        file_obj.write(add_item)
-
-# def read_basket_file():
-#     filename = "user_basket.txt"
-#     with open(filename, 'r') as file_obj:
-#         file_contents = file_obj.readlines()
-    
-#     for item in file_contents:
-#         print(item)
-
-def display_products_in_user_basket():
-
-    if len(basket) == 0:
-        print("There are no items in your basket.")
-    
-    else:
-        print("Here are the items in your basket.")
-        # for product in basket:
-        #     print(product)
-        filename = "user_basket.txt"
-        with open(filename, 'r') as file_obj:
-            file_contents = file_obj.readlines()
-    
-        for item in file_contents:
-            print(item)
-
-
-basket = [] #["fish food", "£2"], ["collar", "£12"]
-products_dict = {}
-products = []
-filename = "store_products_file.txt"
-
-while True:
-    file_contents = read_basket_file()
-    format_store_file_contents()
-    populate_store_products_to_dict()
-    print(products)
+def shop_options():
+    """
+    This function is prompting the user to input their choice from a menu of options.
+    The user's input is then returned as the value of the `user_input` variable.
+    """
     user_input = input("Menu:\n"
-                      "Option 1: View Store items\n"
-                      "Option 2: View your basket\n"
-                      "Option 3: Add items to basket\n"
-                      "Option 4: Remove items from basket\n"
-                      "Option 5: Checkout\n"
-                      "Option 6: Exit\n"
-                      ": ")
-    print('-'*79)
+                    "1: View Store items\n"
+                    "2: View your basket\n"
+                    "3: Add items to basket\n"
+                    "4: Remove items from basket\n"
+                    "5: Checkout\n"
+                    "6: Exit\n"
+                    ": ")
+    return user_input
 
-    if user_input == '1':
-        # read products from text file, format data, populate products into a dict, present products dict to user.
-        display_store_products_to_user()
-        break
+def read_file_contents(file):
+    """
+    The function `read_file_contents` reads the contents of a file and returns them
+    as a list of lines.
+    """
+    filename = file
+    with open(filename, 'r') as file_obj:
+        content = file_obj.readlines()
+    return content
 
-    if user_input == '2':
-        display_products_in_user_basket() # display the items the user has added to their basket.
-        break
+def format_file_contents_and_store_in_product_dict(file_contents):
+    """
+    The function takes in file contents, formats them, and stores them in a
+    dictionary.
+    """
+    for line in file_contents:
+        remove_new_line = line.strip()
+        store_content_as_list = remove_new_line.split(',')
+        product_id = store_content_as_list[0]
+        product_name = store_content_as_list[1].strip()
+        product_price = int(store_content_as_list[2])
+        store_products_dict[product_id] = [product_name, product_price]
+    return store_products_dict
 
-    if user_input == '3':
-        add_item = add_product()
-        if validate_user_product(add_item):
-            item_to_add = obtain_product_and_price(add_item)
-            write_to_basket_file(item_to_add)
-            read_basket_file()
-        else:
-            print(f"{add_item} That item is not in our products list.")
-        break
+def store_file_contents_in_dict(formated_content, store_file_contents_in_dict):
+    """
+    The function takes formatted content and stores it in a dictionary with the
+    product name as the key and a list of the product's attributes as the value.
+    """
+    for product in formated_content:
+        store_file_contents_in_dict[product[0]] = [product[1], product[2]]
+    return store_file_contents_in_dict
 
-    if user_input == '4':
-        break
+def display_products_to_user(store_products_dict):
+    """
+    The function `display_products` takes a dictionary of store products as input
+    and prints the product ID, name, and price for each product.
+    """
+    print("See our products:\n")
+    for key, value in store_products_dict.items():
+        product_id = key
+        product_name = value[0]
+        product_price = value[1]
 
-    if user_input == '5':
-        break
+        print('-'*79)
+        print(f"Product ID: {product_id}\nProduct: {product_name}\nPrice: £{product_price}")
 
-    if user_input == '6':
-        break
+user_basket = []
+store_products_dict = {}
+filename = 'store_products_file.txt'
+file_data = read_file_contents(filename)
+formatted_content = format_file_contents_and_store_in_product_dict(file_data)
 
-    else:
-        print("You have entered an incorrect menu value.")
-        continue
+# Loop starts here
+shopping_finished = False
+while (not shopping_finished):
 
-
-
-
-# if user want to add an item to the basket, press option 2
-# add_item = add_product()
-# validate_user_product(add_item)
-
-# def convert_products_from_file_to_dict(filename):
-#     pass
-
-# with open(filename, 'r') as file_obj:
-#     for line in file_obj:
-#         format_line = line.strip().replace(',', ' ').split()
-#         products.append(format_line)
-
-# print(products)
-
-# for product_index, product in enumerate(products):
-#     product_id = products[product_index][0]
-#     product_name = products[product_index][1]
-#     product_price = products[product_index][2]
-#     products_dict[product_id] = [product_name, product_price]
-
-# print(products_dict)
+    user_option = shop_options()
     
-# file_contents = open_store_file_contents()
-# format_store_file_contents()
-# populate_store_products_to_dict()
-# display_store_products()
-# display_products_in_user_basket()
-# for a user to add an item to the basket, the products list needs to be populated
+    if user_option == '1':
+        print("You selected 'View store items'")
+        display_products = display_products_to_user(store_products_dict)
+
+    elif user_option == '2':
+        print("Do something")
+
+    elif user_option == '3':
+        # the product first needs to be found in the values of a the dict.values to continue.
+        # add items to a basket will first write the product to a text file
+        print("Do something")
+
+    elif user_option == '4':
+        print("Do something")
+
+    elif user_option == '5':
+        print("Do something")
+
+    elif user_option == '6':
+        print("Goodbye.")
+        shopping_finished = True
+    
+    else:
+        print("You have entered an invalid option.")
